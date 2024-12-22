bf6b3fe262113c39d7e10cfe464c1d3ccefd8ee3
dhwpdnr <ink1237@gmail.com>
2024-12-22 15:57:37 +0900

    chore: page routing

--- src/main/java/com/example/spring_test/controller/HomeController.java
+++ src/main/java/com/example/spring_test/controller/HomeController.java
@@ -10,4 +10,24 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/regist/index")
+    public String registIndex() {
+        return "regist_index";
+    }
+
+    @GetMapping("/regist")
+    public String regist() {
+        return "regist";
+    }
+
+    @GetMapping("/auth/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signup(){
+        return "signup";
+    }
 }
